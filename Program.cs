c09f5db2b7eb0c0b7e784f30dda40a00f0612811
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-05-03 20:31:51 -0300

    conseGui

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿int tempo;
+double distancia;
+double velocidadeSom;
+
+velocidadeSom = 340.29;
+
+Console.Clear();
+
+Console.Write("Digite à quantos segundos você ouviu o som do trovão: ");
+tempo = Convert.ToInt32(Console.ReadLine());
+
+distancia = ((tempo * velocidadeSom) / 1000 );
+
+Console.WriteLine($"A sua distância do raio é de {distancia:N5} km: ");
